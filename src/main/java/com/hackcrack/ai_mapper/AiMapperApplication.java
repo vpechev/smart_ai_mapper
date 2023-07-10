82c445e06bcc6162fc5dbbccffaf96bdc14cefb7
boryana.miteva <boryana.miteva@yotpo.com>
2023-07-10 09:38:21 +0300

    feat(ai-mapper): structure

---
+++ src/main/java/com/hackcrack/ai_mapper/AiMapperApplication.java
@@ -0,0 +1,13 @@
+package com.hackcrack.ai_mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMapperApplication.class, args);
+	}
+
+}
